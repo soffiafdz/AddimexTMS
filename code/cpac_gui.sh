328a0a3c0ac7023dbfa44207b3953324e06643a3
soffiafdz <so1.618e@gmail.com>
2019-04-08 22:39:35 -0400

    Deleted some duplicate scripts

--- code/cpac_gui.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-singularity run \
-        -B /run/media/sofdez/Alpha/TMS/bids:/bids_dataset \
-        -B /run/media/sofdez/Alpha/TMS/cpac:/outputs \
-        /home/sofdez/singularity_images/bids_cpac*.img \
-        /bids_dataset \
-        /outputs\
-        GUI
