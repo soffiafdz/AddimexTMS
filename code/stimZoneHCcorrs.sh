01526a236a6559d0231c30c7d23e295dfe140ea3
soffiafdz <so1.618e@gmail.com>
2019-04-09 10:58:08 -0400

    Done script

--- code/stimZoneHCcorrs.sh
+++ code/stimZoneHCcorrs.sh
@@ -1,48 +1,53 @@
 #!/bin/bash
 
-home=$(pwd)
+
 inDir=$1
-seedsDir=$2
-controlsList=$3
+outDir=$2
+seedsDir=$3
+SubsList=$4
 
+usg="Usage: $0 <inDir: directory with the preprocessed BOLD images> \n \
+<outDir: directory to output files> \n \
+<seedsDir: directory with seeds \n \
+<IDSfile: CSV file with sub (and session)>"
 
+if [[ $# -lt 4 ]]; then
+	echo $usg;
+	exit 0;
+else
+    while read subID; do
+        echo "Processing SUBJECT $subID"
+        for seed in $(ls ${seedsDir}/*mniConeNorm.nii.gz); do
+            stimZ=$(basename $seed)
+            stimZid=${stimZ%mniCone*}
 
-while read hcontrol; do
+            for gs in gs0 gs1; do
+                echo "Extracting Time Series for $stimZ (${gs})"
+                mkdir -p ${outDir}/timeSeries/${gs}
+                #FSL
+                fslmeants -i ${inDir}/${subID}_pp_scrub_${gs}_mni.nii.gz \
+                -o ${outDir}/timeSeries/${gs}/${subID}_stimZ${stimZid}_${gs}.1D \
+                -m ${seed}
 
-    echo "Processing SUBJECT $hcontrol"
+                echo "Finished extracting Time Series"
 
-    for seed in $(ls ../seeds3mm/*cone_norm.nii.gz); do
-        stimZ=$(basename $seed)
-        stimZid=${stimZ%_cone*}
-
-        echo "Extracting Time Series for $stimZ"
-
-        fslmeants -i ${hcDir}/${hcontrol}_pp_scrub_gs_mni.nii.gz \
-        -o timeSeries/${hcontrol}_stimZ${stimZid}.1D \
-        -m ${seed}
-
-        echo "Finished extracting Time Series"
-
-        echo "Creating CORRelation maps for $stimZ"
-
-
-        3dfim+ -polort 3 \
-        -input ${hcDir}/${hcontrol}_pp_scrub_gs_mni.nii.gz \
-        -ideal_file timeSeries/${hcontrol}_stimZ${stimZid}.1D \
-        -out Correlation \
-        -bucket correlationMaps/r/${hcontrol}_stimZ${stimZid}_corr.nii.gz
-
-        3dcalc -a correlationMaps/r/${hcontrol}_stimZ${stimZid}_corr.nii.gz \
-        -expr 'log((1+a)/(1-a))/2' \
-        -prefix correlationMaps/z/${hcontrol}_stimZ${stimZid}_corrZ.nii.gz
-
-
-        echo "Done with stimZone $stimZ"
-
-    done
-
-    echo "Done with SUBJECT $hcontrol"
-
-done < $controlsList
-
-echo "##DONE##"
+                echo "Creating CORRelation maps for $stimZ (${gs})"
+                mkdir -p ${outDir}/correlationMaps/r
+                mkdir -p ${outDir}/correlationMaps/z
+                #AFNI
+                3dfim+ -polort 3 \
+                -input ${inDir}/${subID}_pp_scrub_gs_mni.nii.gz \
+                -ideal_file ${outDir}/timeSeries/${subID}_stimZ${stimZid}.1D \
+                -out Correlation \
+                -bucket ${outDir}/correlationMaps/r/${subID}_stimZ${stimZid}_corr.nii.gz
+                #AFNI
+                3dcalc -a ${outDir}/correlationMaps/r/${subID}_stimZ${stimZid}_corr.nii.gz \
+                -expr 'log((1+a)/(1-a))/2' \
+                -prefix ${outDir}/correlationMaps/z/${subID}_stimZ${stimZid}_corrZ.nii.gz
+            done
+            echo "Done with stimZone $stimZ"
+        done
+        echo "Done with SUBJECT $subID"
+    done < $controlsList
+fi
+echo "## DONE ##"
